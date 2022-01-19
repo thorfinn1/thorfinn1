e3259ce92c2f3fc139c6bea64ec20491986e3496
thorfinn1 <58335791+thorfinn1@users.noreply.github.com>
2022-01-19 18:55:02 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   images: {
+    loader: 'cloudinary',
     domains: ['links.papareact.com', 'res.cloudinary.com'],   
+    path: 'https://res.cloudinary.com/dvhcociyf/image/upload/v1642634347/metas/airbnb-clone/57b9f708-bb12-498c-bc33-769f8fc43e63_fik2hy.webp'
   },
   reactStrictMode: true,
 }
