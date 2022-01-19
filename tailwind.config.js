74735ef99d7989fe9435579ab3d0acc4cb3550b4
thorfinn1 <58335791+thorfinn1@users.noreply.github.com>
2022-01-19 18:36:49 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
