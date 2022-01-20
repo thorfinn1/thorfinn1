e93d603ba5b6955152bc931486bc03e9b7704327
thorfinn1 <58335791+thorfinn1@users.noreply.github.com>
2022-01-19 19:23:35 -0500

    Update Banner.js

--- Components/Banner.js
+++ Components/Banner.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 function Banner() {
     return (
         <div className="relative h-[300px] sm:h-[400px] lg:[500px] xl:h-[600px] 2xl:h-[700px]">
-            <Image src="/0fm" layout="fill"
+            <Image src="links.papareact.com/0fm" layout="fill"
             objectFit="cover" 
             />
             <div className="absolute top-1/2 w-full text-center">
