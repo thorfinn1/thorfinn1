20a2ebab40885501ec004731c0282ac650ac018c
thorfinn1 <58335791+thorfinn1@users.noreply.github.com>
2022-01-19 19:05:14 -0500

    Update Banner.js

--- Components/Banner.js
+++ Components/Banner.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 function Banner() {
     return (
         <div className="relative h-[300px] sm:h-[400px] lg:[500px] xl:h-[600px] 2xl:h-[700px]">
-            <Image src="https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg" layout="fill"
+            <Image src="/0fm" layout="fill"
             objectFit="cover" 
             />
             <div className="absolute top-1/2 w-full text-center">
