e1569ed74dbc38a4ff926fcc687cc87d19824f08
thorfinn1 <58335791+thorfinn1@users.noreply.github.com>
2022-01-19 19:06:13 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   images: {
     loader: 'cloudinary',
     domains: ['links.papareact.com', 'res.cloudinary.com'],   
-    path: 'https://cloudinary.com/console/c-a29d29896489e75d51d4e4a7e1ffe6/media_library/folders/home'
+    path: 'https://links.papareact.com'
   },
  
 }
